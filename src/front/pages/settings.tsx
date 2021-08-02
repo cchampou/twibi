e88bcfa84daf5a3485f658a142fecdd74b041cb5
cchampou <clement@champouillon.com>
2021-08-02 23:20:34 +0200

    fix: discord callback url

--- src/front/pages/settings.tsx
+++ src/front/pages/settings.tsx
@@ -5,7 +5,9 @@ import Button from '../components/Button';
 const Settings = () => (
   <section>
     <Heading level={2}>Settings</Heading>
-    <a href="https://discord.com/api/oauth2/authorize?client_id=848550213747474472&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fdashboard&response_type=token&scope=identify">
+    <a
+      href={`https://discord.com/api/oauth2/authorize?client_id=848550213747474472&redirect_uri=${process.env.PUBLIC_URL}%2Fdashboard&response_type=token&scope=identify`}
+    >
       <Button type="button">Connect to Discord</Button>
     </a>
   </section>
