b7ff20342215a43f333e1f1582582c13db6723ef
cchampou <clement@champouillon.com>
2021-08-04 00:13:51 +0200

    refactor: add user type

--- src/back/twitch/models/User.ts
+++ src/back/twitch/models/User.ts
@@ -1,10 +1,15 @@
 import { Schema, model } from 'mongoose';
 
+interface UserType {
+  twitchUsername: string;
+  twitchAccessToken: string;
+}
+
 const userSchema = new Schema({
   twitchUsername: String,
   twitchAccessToken: String,
 });
 
-const User = model('User', userSchema);
+const User = model<UserType>('User', userSchema);
 
 export default User;
