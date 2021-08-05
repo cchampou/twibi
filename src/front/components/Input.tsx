b7f743eb1e78572b0a2b89170a5b9bab80475152
Clément Champouillon <clement@champouillon.com>
2021-08-06 00:03:43 +0200

    feat: some UI rework (#4)

--- src/front/components/Input.tsx
+++ src/front/components/Input.tsx
@@ -1,7 +1,14 @@
 import styled from '@emotion/styled';
+import theme from '../theme';
 
 const Input = styled('input')`
-  padding: 1rem 0.5rem;
+  padding: 0.5rem 1rem;
+  background-color: black;
+  color: ${theme.colors.white};
+  font-weight: bold;
+  margin: 1rem;
+  border-radius: 2px;
+  border: none;
 `;
 
 export default Input;
