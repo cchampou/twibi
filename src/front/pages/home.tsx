57bcf8ef1452449585bdb674f3aafbe11068db01
cchampou <clement@champouillon.com>
2021-08-06 00:01:19 +0200

    feat: some UI rework

--- src/front/pages/home.tsx
+++ src/front/pages/home.tsx
@@ -1,5 +1,8 @@
 import React, { useMemo } from 'react';
 import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import Logo from '../components/Logo';
 import Button from '../components/Button';
 import Heading from '../components/Heading';
@@ -9,6 +12,7 @@ import Picture, { SizeEnum } from '../components/Image';
 import down from '../assets/down.svg';
 import twitch from '../assets/twitch.png';
 import discord from '../assets/discord.png';
+import theme from '../theme';
 
 const HomeWrapper = styled('div')`
   display: flex;
@@ -21,16 +25,41 @@ const HomeWrapper = styled('div')`
 
 const SecondWrapper = styled('div')`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: flex-start;
   align-items: center;
   height: 50vh;
-  width: 100vw;
+  width: 80vw;
+  margin: auto;
+`;
+
+const ThirdWrapper = styled(SecondWrapper)`
+  justify-content: flex-end;
+`;
+
+const UnderWrapper = styled('div')`
+  padding: 5rem;
+`;
+
+const Paragraph = styled('p')`
+  line-height: 3rem;
+`;
+
+const Footer = styled('footer')`
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Chevron = styled(Picture)`
   position: absolute;
   top: 90vh;
+`;
+
+const GithubLink = styled('a')`
+  text-decoration: none;
+  color: ${theme.colors.white};
 `;
 
 const Home = () => {
@@ -53,20 +82,39 @@ const Home = () => {
         <Chevron alt="down" src={down} size={SizeEnum.SMALL} />
       </HomeWrapper>
       <SecondWrapper>
-        <Heading level={2}>Twitch chat notifications</Heading>
-        <p>
-          Get informed when a viewer is follow, hosting, subscribing, and more !
-        </p>
-        <Picture alt="twitch" src={twitch} size={SizeEnum.SMALL} />
+        <UnderWrapper>
+          <Heading level={2}>Twitch chat notifications</Heading>
+          <Paragraph>
+            Get informed when a viewer is follow, hosting, subscribing, and more
+            !
+          </Paragraph>
+        </UnderWrapper>
+        <UnderWrapper>
+          <Picture alt="twitch" src={twitch} size={SizeEnum.SMALL} />
+        </UnderWrapper>
       </SecondWrapper>
-      <SecondWrapper>
-        <Heading level={2}>Now live notifications</Heading>
-        <p>
-          Inform your fans you are live on your favorite social networks, like
-          Discord, Twitter & more.
-        </p>
-        <Picture alt="discord" src={discord} size={SizeEnum.SMALL} />
-      </SecondWrapper>
+      <ThirdWrapper>
+        <UnderWrapper>
+          <Picture alt="discord" src={discord} size={SizeEnum.SMALL} />
+        </UnderWrapper>
+        <UnderWrapper>
+          <Heading level={2}>Now live notifications</Heading>
+          <Paragraph>
+            Inform your fans you are live on your favorite social networks, like
+            Discord, Twitter & more.
+          </Paragraph>
+        </UnderWrapper>
+      </ThirdWrapper>
+      <Footer>
+        <GithubLink
+          href="https://www.github.com/cchampou/twibi"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+          &nbsp;Github&nbsp;
+          <FontAwesomeIcon icon={faExternalLinkAlt} />
+        </GithubLink>
+      </Footer>
     </>
   );
 };
