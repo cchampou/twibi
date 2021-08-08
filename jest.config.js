64bcafdbe4d97e6c088497ea6d1a9f0d4dea4bb7
Clément Champouillon <clement@champouillon.com>
2021-08-08 13:10:19 +0200

    feat(tests): add coverage  (#9)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   transformIgnorePatterns: ['/node_modules/(?!(pupa|escape-goat)).+\\.js$'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx}',
+    '!**/node_modules/**',
+    '!dist/**',
+    '!coverage/**',
+  ],
 };
--- src/back/utils/__tests__/string.spec.ts
+++ src/back/utils/__tests__/string.spec.ts
@@ -1,6 +1,30 @@
-import { insertVariables } from '../string';
+import { insertVariables, splitWords, trimStart } from '../string';
 
 const sampleVariables = { username: 'k_talpa' };
+
+describe('splitWords', () => {
+  test.each([
+    ['toto', ['toto']],
+    ['a very interesting phrase', ['a', 'very', 'interesting', 'phrase']],
+    ['', ['']],
+    [' ', ['', '']],
+    [' . ', ['', '.', '']],
+  ])('splitWords("%s")', (input, output) => {
+    expect(splitWords(input)).toEqual(output);
+  });
+});
+
+describe('trimStart', () => {
+  test.each([
+    ['####toto', '#', 'toto'],
+    ['####toto', ' ', '####toto'],
+    ['## toto', '#', ' toto'],
+    ['.###toto', '.', '###toto'],
+    [' .####toto', '.', ' .####toto'],
+  ])('trimStart("%s", "%s")', (input, char, output) => {
+    expect(trimStart(input, char)).toEqual(output);
+  });
+});
 
 describe('insertVariables', () => {
   it('should return the text as it when no variable slots', () => {
@@ -13,6 +37,14 @@ describe('insertVariables', () => {
   it('should return text with inserted variables', () => {
     expect(insertVariables('Hello {username} !', sampleVariables)).toEqual(
       'Hello k_talpa !'
+    );
+  });
+
+  it('should return original text with bad variable slots', () => {
+    // eslint-disable-next-line no-console
+    console.error = jest.fn();
+    expect(insertVariables('Hello {user} !', sampleVariables)).toEqual(
+      'Hello {user} !'
     );
   });
 });
