5f6d17a30ba9922f5a2d93ed5ca41d4d6b0e625f
cchampou <clement@champouillon.com>
2021-08-08 15:27:42 +0200

    test: cover 2 other http functions

--- src/front/utils/__tests__/http.spec.ts
+++ src/front/utils/__tests__/http.spec.ts
@@ -1,10 +1,16 @@
-import { postRequest } from '../http';
+import { deleteRequest, getJsonRequest, postRequest } from '../http';
 
 (global as any).fetch = jest.fn(() => Promise.resolve('OK'));
 
 const fakeData = { one: 'payload' };
 
+const fakeHeaders = { auth: 'token' };
+
 describe('postRequest', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should call fetch with the correct params', () => {
     postRequest('url', fakeData, {});
     expect(fetch).toBeCalledWith('url', {
@@ -20,7 +26,7 @@ describe('postRequest', () => {
   });
 
   it('should throw in case of error', async () => {
-    (global.fetch as jest.Mock).mockImplementation(() => {
+    (global.fetch as jest.Mock).mockImplementationOnce(() => {
       throw Error('FATAL');
     });
     try {
@@ -28,5 +34,34 @@ describe('postRequest', () => {
     } catch (e) {
       expect(e.message).toEqual('FATAL');
     }
+  });
+});
+
+describe('getJsonRequest', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return a correct JSON', async () => {
+    (global.fetch as jest.Mock).mockImplementationOnce(() =>
+      Promise.resolve({
+        json: jest.fn(() => Promise.resolve('JSON')),
+      })
+    );
+    const res = await getJsonRequest('url', fakeHeaders);
+    expect(res).toEqual('JSON');
+  });
+});
+
+describe('deleteRequest', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('should call fetch with the correct args', async () => {
+    await deleteRequest('url', fakeHeaders);
+    expect(fetch).toBeCalledWith('url', {
+      method: 'DELETE',
+      headers: fakeHeaders,
+    });
   });
 });
