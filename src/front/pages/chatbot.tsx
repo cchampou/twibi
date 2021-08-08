147bcd0bf4329ac03bb2a7d4b76c7e04d0b3cc4f
cchampou <clement@champouillon.com>
2021-08-08 11:49:23 +0200

    feat: add twitch notifications instructions

--- src/front/pages/chatbot.tsx
+++ src/front/pages/chatbot.tsx
@@ -5,6 +5,10 @@ import Notification from '../modules/notification/notification';
 const Chatbot = () => (
   <>
     {/* <Chatbot /> */}
+    <p>
+      To mention the user which is responsible of the event, write
+      {` "{username}"`}
+    </p>
     <Notification type="host" />
     <Notification type="follow" />
     <Notification type="subscribe" />
